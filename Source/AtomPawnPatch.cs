ec123818e464a5869bb175aa0f7253ddde234ecb
Tamas.Szekeres <firefoxpdm@yahoo.co.uk>
2020-03-23 21:04:07 +0100

    1.1 and added the atom bot

---
+++ Source/AtomPawnPatch.cs
@@ -0,0 +1,60 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Verse;
+
+namespace Additional_Robots
+{
+    static class AtomPawnPatch
+    {
+        private static List<WorkGiver> nuclearWorkGivers = new List<WorkGiver>();
+
+        static AtomPawnPatch()
+        {
+            List<WorkTypeDef> allDefsListForReading = DefDatabase<WorkTypeDef>.AllDefsListForReading;
+            allDefsListForReading.ForEach(def =>
+            {
+                if (def.defName.Equals("NuclearWork"))
+                {
+                    def.workGiversByPriority.ForEach(workGiver =>
+                    {
+                        nuclearWorkGivers.Add(workGiver.Worker);
+                    });
+                    return;
+                }
+            });
+        }
+
+        public static bool GetWorkGivers(object __instance, ref List<WorkGiver> __result)
+        {
+            if (atom((Pawn)__instance))
+            {
+                __result = nuclearWorkGivers;
+                return false;
+            }
+
+            return true;
+        }
+
+        public static void TickRare(object __instance)
+        {
+            Log.Error("Tick");
+            Pawn bot = (Pawn)__instance;
+            if (atom(bot))
+            {
+                bot.health.hediffSet.hediffs.ForEach(hediff => {
+                    if (hediff.def.defName.Equals("RadiationMechanoid"))
+                    {
+                        hediff.Severity = 0;
+                    }
+                });
+            }
+        }
+
+        private static bool atom(Pawn bot)
+        {
+            return bot.kindDef.defName.StartsWith("RPP_Bot_Atom");
+        }
+    }
+}
---
+++ Source/Controller.cs
@@ -0,0 +1,37 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System.Reflection;
+using Verse;
+
+namespace Additional_Robots
+{
+    public class Controller : Mod
+    {
+        public Controller(ModContentPack content) : base(content)
+        {
+            Harmony harmony = new Harmony("com.firefox.additionalrobots");
+            //harmony.PatchAll(Assembly.GetExecutingAssembly());
+            harmony.Patch(
+                original: AccessTools.Method(
+                    type: AccessTools.TypeByName("X2_AIRobot"),
+                    name: "GetWorkGivers"
+                    ),
+                prefix: new HarmonyMethod(
+                    methodType: typeof(AtomPawnPatch),
+                    methodName: "GetWorkGivers"
+                    )
+                );
+
+            harmony.Patch(
+               original: AccessTools.Method(
+                   type: typeof(Pawn),
+                   name: "TickRare"
+                   ),
+               prefix: new HarmonyMethod(
+                   methodType: typeof(AtomPawnPatch),
+                   methodName: "TickRare"
+                   )
+               );
+        }
+    }
+}
